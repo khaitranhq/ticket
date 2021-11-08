0a51668d6e44ae9896b3ba60e09db78e4885b7fa
lioaslan <leoalan5577@gmail.com>
2021-11-08 11:34:16 +0700

    Update port

--- ticket-mail/src/main/java/com/crossaz/ticket/TicketMailApplication.java
+++ ticket-mail/src/main/java/com/crossaz/ticket/TicketMailApplication.java
@@ -3,7 +3,7 @@ package com.crossaz.ticket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class TicketMailApplication {
     public static void main(String[] args) {
         SpringApplication.run(TicketMailApplication.class, args);
